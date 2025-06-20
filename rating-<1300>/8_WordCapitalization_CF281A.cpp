7add256582b6cae5815c85addf7186122e3123d5
alidev313xA <ali.dev423@gmail.com>
2025-06-20 16:15:41 +0500

    Add: 8_WordCapitalization_CF281A.cpp

---
+++ rating-<1300>/8_WordCapitalization_CF281A.cpp
@@ -0,0 +1,11 @@
+// https://codeforces.com/problemset/problem/281/A
+#include <bits/stdc++.h>
+using namespace std; 
+
+int main () {
+    string s; 
+    cin >> s; 
+
+    s[0] = toupper(s[0]); 
+    cout << s << "\n"; 
+}
