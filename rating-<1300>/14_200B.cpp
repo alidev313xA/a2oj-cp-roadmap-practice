22753b8342a106b4213476383cfe862da0dae3d3
alidev313xA <ali.dev423@gmail.com>
2025-06-23 17:27:03 +0500

    Add: 14_200B.cpp

---
+++ rating-<1300>/14_200B.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+int main() {
+    int n; cin >> n; 
+    int sum = 0; 
+    int a; 
+    
+    for (int i = 0; i < n; i++) {
+        cin >> a; 
+        sum += a; 
+    }
+    //cout << sum << endl;
+    cout << fixed << setprecision(12) << ((double) sum / (double) (n * 100)) * 100 << endl; 
+}
