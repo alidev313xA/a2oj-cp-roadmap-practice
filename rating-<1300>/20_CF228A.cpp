e1f30a91bb873a935906174d57140517845d8050
alidev313xA <ali.dev423@gmail.com>
2025-06-25 14:06:21 +0500

    Create 20_CF228A.cpp

---
+++ rating-<1300>/20_CF228A.cpp
@@ -0,0 +1,12 @@
+// https://codeforces.com/problemset/problem/228/A
+#include <bits/stdc++.h> 
+using namespace std; 
+
+int main() {
+    unordered_set<int> set; 
+    for (int i = 0; i < 4; i++) {
+        int a; cin >> a; 
+        set.insert(a); 
+    }
+    cout << 4 - set.size() << "\n"; 
+}
