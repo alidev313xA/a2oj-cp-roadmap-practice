3362682fc28749d4eec48f9b4e4de83b45b01462
alidev313xA <ali.dev423@gmail.com>
2025-06-28 16:07:21 +0500

    Add: 27_CF231A.cpp

--- rating-<1300>/27_CF231A.cpp
+++ rating-<1300>/27_CF231A.cpp
@@ -1,1 +1,16 @@
+// https://codeforces.com/problemset/problem/231/A
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    int n, res = 0;
+    cin >> n;
+
+    while (n--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        res += (a + b + c >= 2);
+    }
+
+    cout << res << "\n";
+}
