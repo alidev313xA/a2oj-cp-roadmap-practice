09a09f68ba29bb30a93daf0a0e032e3a32a80be6
alidev313xA <ali.dev423@gmail.com>
2025-07-02 16:40:19 +0500

    Add: 37_CF199A.cpp

---
+++ rating-<1300>/37_CF199A.cpp
@@ -0,0 +1,11 @@
+// https://codeforces.com/problemset/problem/199/A
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    cout << "0 0 " << n << "\n";
+    return 0;
+}
